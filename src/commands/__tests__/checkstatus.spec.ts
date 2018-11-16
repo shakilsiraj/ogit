31874a1742c4e2f0eeb6f9522c1fba2ae733c56e
Shakil Siraj <shakil.siraj@gmail.com>
2018-11-16 12:07:31 +0000

    fixed test

--- src/commands/__tests__/checkstatus.spec.ts
+++ src/commands/__tests__/checkstatus.spec.ts
@@ -1,18 +1,17 @@
-import CheckStatusCommand from "../checkstatus";
+import CheckStatusCommand from "../status-all";
 
-describe('ogit', () => {
-    describe('commands', () => {
-        describe('chekstatus', () => {
-            it('instnace should be created', () => {
-                expect(new CheckStatusCommand(null, null)).toBeDefined();
-            });
-            it('description should be present', () => {
-                expect(CheckStatusCommand.description).toBeDefined();
-            });
-            it('aliases should be defined', () => {
-                expect(CheckStatusCommand.aliases[0]).toEqual('status');
-            });
-
-        });
+describe("ogit", () => {
+  describe("commands", () => {
+    describe("chekstatus", () => {
+      it("instnace should be created", () => {
+        expect(new CheckStatusCommand(null, null)).toBeDefined();
+      });
+      it("description should be present", () => {
+        expect(CheckStatusCommand.description).toBeDefined();
+      });
+      it("aliases should be defined", () => {
+        expect(CheckStatusCommand.aliases[0]).toEqual("status");
+      });
     });
+  });
 });
