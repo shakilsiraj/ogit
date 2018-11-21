f155f60fad5d5de6eecd87a8692b8ae92e9e794d
Shakil Siraj <shakil.siraj@gmail.com>
2018-11-21 12:32:02 +0000

    Removing console.log

--- src/commands/display-changes.ts
+++ src/commands/display-changes.ts
@@ -1,64 +1,63 @@
-import { ChangeTypes } from '../models/GitStatus';
-import 'reflect-metadata';
-const terminalLink = require('terminal-link');
-const chalk = require('chalk');
-const columnify = require('columnify');
-import { GitWrapper } from '../wrapper/git';
+import { ChangeTypes } from "../models/GitStatus";
+import "reflect-metadata";
+const terminalLink = require("terminal-link");
+const chalk = require("chalk");
+const columnify = require("columnify");
+import { GitWrapper } from "../wrapper/git";
 import { Command, flags } from "@oclif/command";
-import { GitStatus } from '../models';
+import { GitStatus } from "../models";
 
 export default class DisplayChangesCommand extends Command {
   static description = "Display all the uncommitted changes";
 
-  static aliases = ['status'];
+  static aliases = ["status"];
 
   async run() {
-
     const status: GitStatus = await GitWrapper.status();
-    const remoteBranch = !!status.trackingBranch ? await GitWrapper.originUrl() : null;
+    const remoteBranch = !!status.trackingBranch
+      ? await GitWrapper.originUrl()
+      : null;
 
     let message = `Your are on branch ${chalk.blue(status.currentBranch)}`;
     if (remoteBranch) {
-      message += ' tracking ';
-      message += chalk.yellow(terminalLink(status.trackingBranch, remoteBranch));
+      message += " tracking ";
+      message += chalk.yellow(
+        terminalLink(status.trackingBranch, remoteBranch)
+      );
     }
 
     console.log(message);
 
     const dataTable = [];
-    console.log(status.created);
     for (let file of status.created) {
       dataTable.push({
-        change: 'New',
+        change: "New",
         path: file.path
-      })
+      });
     }
     for (let file of status.added) {
       dataTable.push({
-        change: 'Added',
+        change: "Added",
         path: file.path
-      })
+      });
     }
     for (let file of status.modified) {
       dataTable.push({
-        change: 'Modified',
+        change: "Modified",
         path: file.path
-      })
+      });
     }
     for (let file of status.deleted) {
       dataTable.push({
-        change: 'Deleted',
+        change: "Deleted",
         path: file.path
-      })
+      });
     }
 
     if (dataTable.length > 0) {
-      console.log(columnify(dataTable, {
-
-      }));
+      console.log(columnify(dataTable, {}));
     } else {
-      console.log(chalk.yellow('You do not have any uncommitted changes'));
+      console.log(chalk.yellow("You do not have any uncommitted changes"));
     }
-
   }
 }
