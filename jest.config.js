35ecbb63b2d701eb057eeb507618311be9b8af3c
Shakil Siraj <shakil.siraj@humanservices.gov.au>
2018-11-22 20:06:03 +1100

    Added more tests for GitWrapper

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,33 @@
 module.exports = {
-    "globals": {
-        "ts-jest": {
-            "diagnostics": true,
-            "tsConfig": "tsconfig.spec.json"
-        },
+  "globals": {
+    "ts-jest": {
+      "diagnostics": true,
+      "tsConfig": "tsconfig.spec.json"
     },
-    "testPathIgnorePatterns": [
-        "<rootDir>/.c9/", "<rootDir>/node_modules/"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "src/**/*.ts"
-    ],
-    "coverageReporters": ["text-summary"],
-    "verbose": true,
-    "transformIgnorePatterns": [
-        "/node_modules/(?!@tsed/testing).+\\.js$"
-    ],
-    "testEnvironment": "node"
+  },
+  "testPathIgnorePatterns": [
+    "<rootDir>/.c9/", "<rootDir>/node_modules/"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "src/**/*.ts"
+  ],
+  "coverageReporters": ["text-summary"],
+  "verbose": true,
+  "transformIgnorePatterns": [
+    "/node_modules/(?!@tsed/testing).+\\.js$"
+  ],
+  "testEnvironment": "node"
 }
--- src/wrapper/__tests__/git.spec.ts
+++ src/wrapper/__tests__/git.spec.ts
@@ -1,19 +1,57 @@
 import 'reflect-metadata';
-import { GitStatus } from './../../models/GitStatus';
 import { GitWrapper } from '../git';
-import { ObjectMapper } from 'json-object-mapper';
+import * as fs from 'fs';
+import * as SimpleGit from 'simple-git/promise';
+import uuid = require('uuid');
+import { __await } from 'tslib';
 
 describe('ogit', () => {
   describe('wrapper', () => {
     describe('git', () => {
       describe('status', () => {
-        it('should return a value', async () => {
+        it('should return a list of created files when a file has created', async done => {
+          const fileName = uuid.v4() + '.txt';
+          const fd = createAndWriteToFile(fileName);
           const status = await GitWrapper.status();
-          expect(status).toBeDefined();
+          status.created.forEach(async stat => {
+            if (stat.path === fileName) {
+              fs.unlinkSync(fileName);
+              done();
+            }
+          });
         });
-        it('should return a list of changed files', async () => {
+        it('should return a list of changed files when a file has changed', async done => {
+          fs.appendFileSync('tslint.json', '{}');
           const status = await GitWrapper.status();
-          expect(status.modified.length > 0).toBeTruthy();
+          status.modified.forEach(async stat => {
+            if (stat.path === 'tslint.json') {
+              await SimpleGit().raw(['checkout', '--', 'tslint.json']);
+              done();
+            }
+          });
+        });
+        it('should return a list of deleted files when a file has been deleted', async done => {
+          fs.unlinkSync('tslint.json');
+          const status = await GitWrapper.status();
+          status.deleted.forEach(async stat => {
+            if (stat.path === 'tslint.json') {
+              await SimpleGit().raw(['checkout', '--', 'tslint.json']);
+              done();
+            }
+          });
+        });
+        it('should return a list of added files when a file has been added', async done => {
+          const fileName = uuid.v4() + '.txt';
+          const fd = createAndWriteToFile(fileName);
+          await SimpleGit().raw(['add', '.']);
+          const status = await GitWrapper.status();
+          status.added.forEach(async stat => {
+            if (stat.path === fileName) {
+              await SimpleGit().raw(['reset', fileName]);
+              fs.unlinkSync(fileName);
+              done();
+            }
+          });
         });
       });
       describe('originUrl', () => {
@@ -61,6 +99,54 @@ describe('ogit', () => {
           }
         });
       });
+
+      describe('optimizeRepo', () => {
+        it('should not fail', async () => {
+            let success = true;
+            try{
+              await GitWrapper.optimizeRepo();
+            }catch(err){
+              success = false;
+            }
+            expect(success).toBeTruthy();
+        });
+      });
+
+      describe('addToRepo', () => {
+        it('should be able to add new files', async () => {
+          const file1 = uuid.v4() + '.txt';
+          createAndWriteToFile(file1);
+          await GitWrapper.addToRepo(file1);
+          const file2 = uuid.v4() + '.txt';
+          createAndWriteToFile(file2);
+          await GitWrapper.addToRepo(file2);
+          const file3 = uuid.v4() + '.txt';
+          createAndWriteToFile(file3);
+          await GitWrapper.addToRepo(file3);
+          const status = await GitWrapper.status();
+          let found = 0;
+          for (let i = 0; i < status.added.length; i++) {
+            if (
+              status.added[i].path === file1 ||
+              status.added[i].path === file2 ||
+              status.added[i].path === file3
+            )
+              await SimpleGit().raw(['reset', status.added[i].path]);
+            found++;
+          }
+          fs.unlinkSync(file1);
+          fs.unlinkSync(file2);
+          fs.unlinkSync(file3);
+          expect(found).toBe(3);
+        });
+      });
     });
   });
 });
+
+function createAndWriteToFile(fileName: string): any {
+  const fd = fs.openSync(fileName, 'w');
+  fs.writeFileSync(fileName, 'blah!');
+  fs.closeSync(fd);
+  return fd;
+}
