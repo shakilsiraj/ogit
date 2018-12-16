586e239a8a87573b68a60215569e1eb5e7d902a2
Shakil Siraj <shakil.siraj@gmail.com>
2018-12-16 12:23:32 +0000

    setting up jest debug configuration for c9

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "globals": {
+        "ts-jest": {
+            "diagnostics": true,
+            "tsConfig": "<rootDir>/tsconfig.spec.json"
+        },
+    },
+    "testPathIgnorePatterns": [
+        "<rootDir>/.c9/", "<rootDir>/node_modules/"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/spec/.*|(\\.|/)(test|spec))\\.ts?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "collectCoverage": true,
+    "coverageReporters": ["text-summary"],
+    "verbose": true,
+    "transformIgnorePatterns": [
+        "/node_modules/(?!@tsed/testing).+\\.js$"
+    ]
+}
