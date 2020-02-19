ad916f299c7c5f2bfc913edac4a8de6937d2ea70
Shakil Siraj <shakil.siraj@gmail.com>
2020-02-19 13:35:43 +1100

    modified create-tag to add pushing tag to origin question

--- src/commands/create-tag.ts
+++ src/commands/create-tag.ts
@@ -7,47 +7,35 @@ export default class CreateTagCommand extends Command {
   static description =
     'Tags the current repository. Does annotated tagging only';
 
-  static flags = {
-    // tag name: --tag or -t
-    tag: flags.string({
-      char: 't',
-      description: 'name of the tag'
-    }),
-    //commit message: --message or -m
-    message: flags.string({
-      char: 'm',
-      description: 'commit message for the tag'
-    })
-  };
-
   async run() {
-    const { flags } = this.parse(CreateTagCommand);
-    const prompts = [];
-    let answers: any = {};
-    if (!flags.tag) {
-      prompts.push({
+    const prompts = [
+      {
         message: 'Please enter the name of the tag',
         type: 'input',
         name: 'tag',
         validate: (tagName: string) => {
           return tagName && tagName.length > 0;
         }
-      });
-      if (!flags.message) {
-        prompts.push({
-          message: 'Please enter the commit message',
-          type: 'input',
-          name: 'message',
-          validate: (message: string) => {
-            return message && message.length > 0;
-          }
-        });
+      },
+      {
+        message: 'Please enter the commit message',
+        type: 'input',
+        name: 'message',
+        validate: (message: string) => {
+          return message && message.length > 0;
+        }
+      },
+      {
+        message: 'Would you like to push the tag to origin?',
+        type: 'confirm',
+        name: 'immediatePush',
+        default: false
       }
-      if (prompts.length > 0) {
-        answers = await inquirer.prompt(prompts);
-      }
+    ];
+    const answers: any = await inquirer.prompt(prompts);
+    await GitFacade.addTag(answers.tag, answers.message);
+    if (answers.immediatePush) {
+      await GitFacade.pushTag(answers.tag);
     }
-    const options = { ...flags, ...answers };
-    await GitFacade.addTag(options.tag, options.message);
   }
 }
