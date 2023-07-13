0d0ae47495919011398ada4b882f4e21312408b0
Shakil Siraj <shakilsiraj@users.noreply.github.com>
2023-07-14 00:23:43 +1000

    Revert "Release/1.28.1"

--- src/utils/Constants.ts
+++
@@ -1,6 +0,0 @@
-export const PRODUCTION_RELEASE_BRANCH = 'ogit.productionReleaseBranch';
-export const NEXT_RELEASE_BRANCH = 'ogit.nextReleaseBranch';
-export const FEATURE_BRNACH_NAME = 'ogit.featureBranchName';
-export const RELEASE_BRANCH_NAME = 'ogit.releaseBranchName';
-export const HOTFIX_BRANCH_NAME = 'ogit.hotfixBranchName';
-export const VERSION_TAG_PREFIX = 'ogit.versionTagPrefix';
